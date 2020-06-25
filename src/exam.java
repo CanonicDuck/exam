9fa61d6cf3f26d377d92e302978084715155766b
Goose <canonicduck@tuta.io>
2020-06-25 13:34:07 +0300

    ЧАСТЬ 1

--- src/exam.java
+++ src/exam.java
@@ -1,59 +1,26 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.*;
-import javafx.stage.Stage;
+import java.util.Random;
 
-public class exam extends Application {
+public class exam {
+    public static void main(String[] args) throws Exception{
+        PGMImage imRand = new PGMImage(80,60);
+        PGMImage imGrad = new PGMImage(80,60);
 
+        for (int i = 0; i < imRand.width; i++) {
+            for (int j = 0; j < imRand.height; j++) {
+                imRand.setPixel(i, j, (int) (Math.random()*256));
+            }
+        }
 
+        for (int i = 0; i < imGrad.width; i++) {
+            for (int j = 0; j < imGrad.height; j++) {
+                imGrad.setPixel(i, j, (i+j) % 256);
+            }
+        }
 
-    /* экзаменационный коммит */
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setScene(new Scene(initInterface(), 640, 800));
-        primaryStage.show();
+        imRand.saveTo("imRand.pgm");
+        imGrad.saveTo("imGrad.pgm");
+
     }
-
-
-    private VBox initInterface() {
-        VBox window = new VBox();
-        HBox h1 = new HBox();
-
-        ImageView iv1 = new ImageView();
-        ImageView iv2 = new ImageView();
-        ImageView iv3 = new ImageView();
-
-        WritableImage im1 = new WritableImage(120, 100);
-        for (int i = 0; i < 120; i++) {
-            for (int j = 0; j < 100; j++) {
-                im1.getPixelWriter().setColor(i,j, Color.GREEN);
-            }
-        }
-        iv1.setImage(im1);
-
-        WritableImage im2 = new WritableImage(256, 256);
-        for (int i = 0; i < 256; i++) {
-            for (int j = 0; j < 256; j++) {
-                im2.getPixelWriter().setColor(i,j, Color.rgb(0, i, j));
-            }
-        }
-        iv2.setImage(im2);
-
-        WritableImage im3 = new WritableImage(256, 256);
-        for (int i = 0; i < 256; i++) {
-            for (int j = 0; j < 256; j++) {
-                im3.getPixelWriter().setColor(i,j, Color.rgb(i, i, j));
-            }
-        }
-        iv3.setImage(im3);
-
-        h1.getChildren().addAll(iv1, iv2, iv3);
-        window.getChildren().addAll(h1);
-        return window;
-    }
-
 }
 
 
