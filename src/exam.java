b876cc7bf28a2d00dde4c82209331def71eb4f64
Goose <canonicduck@tuta.io>
2020-06-25 14:07:45 +0300

    ЧАСТЬ 1 v2

--- src/exam.java
+++ src/exam.java
@@ -1,9 +1,8 @@
-import java.util.Random;
-
 public class exam {
     public static void main(String[] args) throws Exception{
         PGMImage imRand = new PGMImage(80,60);
         PGMImage imGrad = new PGMImage(80,60);
+        PGMImage imTest = new PGMImage(32,8);
 
         for (int i = 0; i < imRand.width; i++) {
             for (int j = 0; j < imRand.height; j++) {
@@ -17,8 +16,19 @@ public class exam {
             }
         }
 
+        int k = 0;
+        for (int i = 0; i < imTest.height; i++) {
+            for (int j = 0; j < imTest.width; j++) {
+                imTest.setPixel(j, i, k);
+                ++k;
+            }
+        }
+        imTest.setPixel(1, 3, 0);
+
+
         imRand.saveTo("imRand.pgm");
         imGrad.saveTo("imGrad.pgm");
+        imTest.saveTo("imTest.pgm");
 
     }
 }
